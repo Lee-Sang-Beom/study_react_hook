a0807e5efab0e96be4a842b29993104f073bf205
Lee-Sang-Beom <tkdqja975@naver.com>
2022-08-01 21:56:16 +0900

    study useRef

--- src/App.js
+++ src/App.js
@@ -1,23 +1,44 @@
 import logo from './logo.svg';
 import './App.css';
+import {useState, useRef, useEffect} from "react";
 
 function App() {
+
+  useEffect (()=>{
+    inputRef.current.focus(); // 페이지 로딩 시, 자동 focus
+  },[])
+
+  const [count, setCount] = useState(0); // count와 ref차이를 비교
+  const [values, setValues] = useState("");
+
+  const stateRef = useRef(0);
+  const inputRef = useRef();
+
+  const countOut =()=>{
+    setCount(count+1);
+    console.log(`count = ${count}`);
+  }
+
+  const refOut = () => {
+    stateRef.current += 1;
+    console.log(`ref = ${stateRef.current}`);
+  }
+
+  const login = ()=>{
+    alert(`어서오세요, ${inputRef.current.value}`)
+  }
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div>
+      <p>count: {count}</p>
+      <p>ref : {stateRef.current} </p>
+      <button onClick={countOut}>add count</button>
+      <button onClick={refOut}>add count</button>
+
+      <div>
+      <input ref={inputRef} type="text"/>
+      <button onClick={login}>he</button>
+      </div>
     </div>
   );
 }
